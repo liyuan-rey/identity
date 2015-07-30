e901d13dce7433944645f6001da862bd1a706ded
liyuan-rey <liyuan-rey@users.noreply.github.com>
2015-07-30 12:03:30 +0800

    add identity server front-end with refresh token support.

---
+++ src/server-frontend/app/app.js
@@ -0,0 +1,46 @@
+﻿var app = angular.module('identityServerFrontend', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar']);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "/app/views/home.html"
+    });
+
+    $routeProvider.when("/login", {
+        controller: "loginController",
+        templateUrl: "/app/views/login.html"
+    });
+
+    $routeProvider.when("/signup", {
+        controller: "signupController",
+        templateUrl: "/app/views/signup.html"
+    });
+
+    $routeProvider.when("/profile", {
+        controller: "profileController",
+        templateUrl: "/app/views/profile.html"
+    });
+
+    $routeProvider.when("/refresh", {
+        controller: "refreshController",
+        templateUrl: "/app/views/refresh.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/home" });
+});
+
+
+var serviceBase = 'http://localhost:12345/';
+app.constant('isfeSettings', {
+    apiServiceBaseUri: serviceBase,
+    clientId: 'employeemanage'
+});
+
+app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptorService');
+});
+
+app.run(['authService', function (authService) {
+    authService.fillAuthData();
+}]);
---
+++ src/server-frontend/app/services/profileService.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+app.factory('profileService', ['$http', 'isfeSettings', function ($http, isfeSettings) {
+
+    var serviceBase = isfeSettings.apiServiceBaseUri;
+    var profileServiceFactory = {};
+
+    var _getProfile = function () {
+
+        return $http.get(serviceBase + 'api/profile').then(function (results) {
+            return results;
+        });
+    };
+
+    profileServiceFactory.getProfile = _getProfile;
+
+    return profileServiceFactory;
+
+}]);
